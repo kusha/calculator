b79551f59558c14885b9d54d5bbc9f6d630b47d0
Martin Knotek <xknote10@stud.fit.vutbr.cz>
2015-04-27 02:25:44 +0200

    Update mathlib.py
    Predelani substituce funkci - jeste neni uplne hotovo
    Ted se musi zanorene funkce zavorkovat (napr. 2^3! se musi zapsat jako (2^3)!

--- mathlib.py
+++ mathlib.py
@@ -1,9 +1,9 @@
 import math
 import re
- 
+
 math_namespace = vars(math).copy()
 math_namespace['__builtins__'] = None
- 
+
 # regex_function = r'(\d+!|\d+(\.\d+)?\^\d+|\|[+,-]?\d+(\.\d+)?\|)'
 # regex_value = r'((([1-9]\d*|0)(\.\d+)?)|'+regex_function+')'
 # regex_operation = r'(\+|\-|\*|\/)'
@@ -13,23 +13,38 @@ tag = r"(\#[1-9][0-9]?)"
 left = r"[\(\|]"
 right = r"[\)\|\!]"
 operands = r"[\+\-\*\/\^]"
- 
+
 value = left+r"*("+digit+r"|"+tag+r")"+right+r"*"
 unit = r"("+value+r"|("+operands+value+r"))*"
- 
+
+dig=r"\d+(\.\d+)?";
+
 regex_all = r"^"+value+r"("+operands+value+r")*$"
 print(regex_all)
 valid_pattern = re.compile(regex_all)
- 
+
 def evaluate(expression, story):
     expression = expression
     namespace = math_namespace
     try:
+        #print(regex_all)
+        #print("Vyraz: "+expression)
         if valid_pattern.match(expression):
             print("VALID",end=" ")
-            expression = re.sub(r'('+unit+')!', "factorial("+r'\1'+")", expression)
-            expression = re.sub(r'(\d+(\.\d+)?)\^(\d+)', "pow("+r'\1'+","+r'\3'+")", expression)
-            expression = re.sub(r'\|[+,-]?(\d+(\.\d+)?)\|', r'\1', expression)
+            expression = "("+expression+")"
+            while(True):
+              oldexpr=expression
+              subexpr=re.sub(r'(.*)(\([^\(\)]+\))(.*)', r'\2', expression)
+              subexpr = re.sub(r'('+dig+')\^(\d+)', "(pow("+r'\1'+","+r'\3'+"))", subexpr)
+              subexpr = re.sub(r'('+dig+')!', "(factorial("+r'\1'+"))", subexpr)
+              subexpr = re.sub(r'\|('+digit+')\|', "(fabs("+r'\1'+"))", subexpr)
+              subresult = eval(subexpr, namespace)
+              subresult = int(subresult) if int(subresult) == float(subresult) else round(float(subresult),8)
+              expression=re.sub(r'(.*)(\([^\(\)]+\))(.*)', r'\1'+"\""+str(subresult)+"\""+r'\3', expression)
+              expression=re.sub("\""+str(subresult)+"\"",str(subresult), expression)
+              if (oldexpr==expression):
+                break
+
             # substitution for story
             if re.findall(r'#([1-9][10-99])', expression):
                 # print(namespace["story"])
