58fea34a12d42c766924911a6c0683329c07088a
Mark Birger <birgerm@yandex.ru>
2015-04-27 22:26:05 +0200

    Matematicka knihovna upravena

--- mathlib.py
+++ mathlib.py
@@ -1,66 +1,110 @@
+#!/usr/bin/env python3
+
+"""
+Math library for calculator.
+"""
+
+__author__ = "Mark Birger, Daniil Khudiakov, Martin Knotek"
+__date__ = "26 Apr 2015"
+__credits__ = ["Mark Birger", "Daniil Khudiakov", "Martin Knotek"]
+
+__license__ = "GNU General Public License v3.0"
+__version__ = "1.0"
+__maintainer__ = "Mark Birger"
+__status__ = "Development"
+
 import math
 import re
 
-math_namespace = vars(math).copy()
-math_namespace['__builtins__'] = None
-#!/usr/bin/env python3
+MATH_NAMESPACE = vars(math).copy()
+MATH_NAMESPACE['__builtins__'] = None
 
-# regex_function = r'(\d+!|\d+(\.\d+)?\^\d+|\|[+,-]?\d+(\.\d+)?\|)'
-# regex_value = r'((([1-9]\d*|0)(\.\d+)?)|'+regex_function+')'
-# regex_operation = r'(\+|\-|\*|\/)'
-# regex_all = '^[+,-]?('+regex_value+regex_operation+')*'+regex_value+"$"
-digit = r"([\-\+]?\d+(\.\d+)?)"
-tag = r"(\#[1-9][0-9]?)"
-left = r"[\(\~]"
-right = r"[\)\!]"
-operands = r"[\+\-\*\/\^\%]"
+DIGIT = r"([\-\+]?\d+(\.\d+)?)"
+LEFT = r"[\(\~]"
+RIGHT = r"[\)\!]"
+OPERANDS = r"[\+\-\*\/\^\%]"
 
-value = left+r"*("+digit+r"|"+tag+r")"+right+r"*"
-unit = r"("+value+r"|("+operands+value+r"))*"
+DIG = r"\d+(\.\d+)?"
+VALUE = LEFT+r"*"+DIGIT+RIGHT+r"*"
 
-dig=r"((\d+(\.\d+)?)|(\#[1-9][0-9]?))";
+# TODO: expression for substitution feature
+# TAG = r"(\#[1-9][0-9]?)"
+# DIG = r"\d+(\.\d+)?"
+# VALUE = LEFT+r"*("+DIGIT+r"|"+TAG+r")"+RIGHT+r"*"
 
-regex_all = r"^"+value+r"("+operands+value+r")*$"
-print(regex_all)
-valid_pattern = re.compile(regex_all)
+UNIT = r"("+VALUE+r"|("+OPERANDS+VALUE+r"))*"
+REGEX_ALL = r"^"+VALUE+r"("+OPERANDS+VALUE+r")*$"
+VALID_PATTERN = re.compile(REGEX_ALL)
 
 def evaluate(expression, story):
+    """
+    Function evaluates literal. Full syntax description in user manual.
+    @param expression string with an expression
+    @param story array with another calculations (TODO)
+    @result string with result or None if input is invalid
+    """
+    if expression is None:
+        return None
+    print("STORY:", story)
     expression = expression
-    namespace = math_namespace
+    namespace = MATH_NAMESPACE
+    namespace["evaluate"] = evaluate
+    namespace["story"] = story
     try:
-        #print(regex_all)
-        #print("Vyraz: "+expression)
-        if valid_pattern.match(expression):
-            print("VALID",end=" ")
+        if VALID_PATTERN.match(expression):
             expression = "("+expression+")"
-            while(True):
-              expression=re.sub(r'(\~('+digit+'))', "("+r'\1'+")", expression)
-              expression=re.sub(r'('+dig+'!)', "("+r'\1'+")", expression)
-              expression=re.sub(r'('+dig+'\^('+digit+'))', "("+r'\1'+")", expression)
-              #print("expression:"+expression)
-              oldexpr=expression
-              subexpr=re.sub(r'(.*)(\([^\(\)]+\))(.*)', r'\2', expression)
-              subexpr=re.sub(r'\~('+digit+')', "floor("+r'\1'+")", subexpr)
-              subexpr = re.sub(r'('+dig+')\^('+digit+')', "pow("+r'\1'+","+r'\3'+")", subexpr)
-              subexpr = re.sub(r'('+dig+')!', "factorial("+r'\1'+")", subexpr)
-              subresult = eval(subexpr, namespace)
-              subresult = int(subresult) if int(subresult) == float(subresult) else round(float(subresult),8)
-              expression=re.sub(r'(.*)(\([^\(\)]+\))(.*)', r'\1'+"\""+str(subresult)+"\""+r'\3', expression)
-              expression=re.sub("\""+str(subresult)+"\"",str(subresult), expression)
-              if (oldexpr==expression):
-                break
+            while True:
+                expression = re.sub(
+                    r'(\~('+DIGIT+'))', "("+r'\1'+")", expression)
+                expression = re.sub(r'('+DIG+'!)', "("+r'\1'+")", expression)
+                expression = re.sub(
+                    r'('+DIG+r'\^('+DIGIT+'))', "("+r'\1'+")", expression)
+                oldexpr = expression
+                subexpr = re.sub(r'(.*)(\([^\(\)]+\))(.*)', r'\2', expression)
+                subexpr = re.sub(r'\~('+DIGIT+')', "floor("+r'\1'+")", subexpr)
+                subexpr = re.sub(
+                    r'('+DIG+r')\^('+DIGIT+')',
+                    "pow("+r'\1'+","+r'\3'+")", subexpr)
+                subexpr = re.sub(r'('+DIG+')!', "factorial("+r'\1'+")", subexpr)
 
-            # substitution for story
-            if re.findall(r'#([1-9][10-99])', expression):
+                # TODO: expression for substitution feature
+                # substitution for story
+                # if re.findall(r'\#[1-9][0-9]?', subexpr):
+                    # namespace["story"] = [None] + \
+                    # [float(i[1]) if i[1] is not None else i[1] for i in story]
+                    # print(namespace["story"])
+                    # print(namespace["story"][1])
+                    # subexpr=re.sub(r'\#([1-9][0-9]?)',
+                    # r'evaluate(story[\1], story)',subexpr)
+                    # print("INPUT EVAL:", subexpr,end=" ")
+
+                subresult = eval(subexpr, namespace)
+
+                subresult = int(subresult) \
+                    if int(subresult) == float(subresult) \
+                    else round(float(subresult), 8)
+                expression = re.sub(
+                    r'(.*)(\([^\(\)]+\))(.*)',
+                    r'\1'+"\""+str(subresult)+"\""+r'\3', expression)
+                expression = re.sub(
+                    "\""+str(subresult)+"\"",
+                    str(subresult), expression)
+                if oldexpr == expression:
+                    break
+
+            # TODO: expression for substitution feature
+            # if re.findall(r'\#[1-9][0-9]?', expression):
                 # print(namespace["story"])
-                namespace["story"] = [None] + [float(i[1]) if i[1] is not None else i[1] for i in story]
-                print(namespace["story"])
-                expression=re.sub(r'#([\d]{1,2})',r'evaluate(story[\1])',expression)
-            print("INPUT:", expression, end=" => ")
+                # namespace["story"] = [None] + [float(i[1]) \
+                #   if i[1] is not None else i[1] for i in story]
+                # print(namespace["story"])
+                # expression=re.sub(r'\#([1-9][0-9]?)', \
+                #   r'evaluate(story[\1])',expression)
             result = eval(expression, namespace)
-            result = int(result) if int(result) == float(result) else round(float(result),8)
+            result = int(result) if int(result) == float(result)\
+                else round(float(result), 8)
             print("RESULT:", result)
             return str(result)
         return None
-    except Exception as e:
+    except Exception:
         return None
